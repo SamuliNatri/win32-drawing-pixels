74b17a43679b09c50706b1dec547ce943b798f4b
Samuli Natri <samuli.natri@gmail.com>
2024-03-15 22:21:11 +0200

    .

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 @echo off
-cl main.c /Zi /nologo /Fea.exe ^
-user32.lib gdi32.lib
+cl main.c /Zi /nologo /Fea.exe
--- main.c
+++ main.c
@@ -1,36 +1,40 @@
+// cl main.c /Zi /nologo
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdint.h>
 
-typedef uint32_t u32;
-void* Memory;
-int ClientWidth;
-int ClientHeight;
+#define Assert(c) do { if (!(c)) __debugbreak(); } while (0)
 
-void DrawPixel(int X, int Y, u32 Color) {
-    u32 *Pixel = (u32 *)Memory;
-    Pixel += Y * ClientWidth + X;
-    *Pixel = Color;
+#pragma comment (lib, "user32.lib")
+#pragma comment (lib, "gdi32.lib")
+
+typedef uint32_t U32;
+
+void *memory;
+U32 client_width;
+U32 client_height;
+
+void draw_pixel(U32 x, U32 y, U32 color) {
+    U32 *pixel = (U32 *)memory;
+    pixel += y * client_width + x;
+    *pixel = color;
 }
 
-void ClearScreen(u32 Color) {
-    u32 *Pixel = (u32 *)Memory;
-    for(int Index = 0; 
-        Index < ClientWidth * ClientHeight; 
-        ++Index) 
-    {
-        *Pixel++ = Color;
+void clear_screen(U32 color) {
+    U32 *pixel = (U32 *)memory;
+    for(U32 i = 0; i < client_width * client_height; ++i) {
+        *pixel++ = color;
     }
 }
 
-LRESULT CALLBACK 
-WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
-{
-    switch(Message) {
+LRESULT CALLBACK win_proc(HWND window, UINT msg, 
+                          WPARAM w_param, LPARAM l_param) {
+    switch(msg) {
         case WM_KEYDOWN: {
-            switch(WParam) {
+            switch(w_param) {
                 case 'O': { 
-                    DestroyWindow(Window); 
+                    DestroyWindow(window); 
                 } break;
             }
         } break;
@@ -40,106 +44,90 @@ WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
         } break;
         
         default: {
-            return DefWindowProc(Window, Message, WParam, LParam);
+            return DefWindowProcW(window, msg, w_param, l_param);
         }
     }
     
     return 0;
 }
 
-int WINAPI 
-WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int CmdShow) {
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, 
+                   PSTR cmd, int cmd_show) {
     
-    // Window creation
+    // Window creation.
     
-    WNDCLASS WindowClass = {0};
+    WNDCLASSW wc = {
+        .lpszClassName = L"MyWindowClass",
+        .lpfnWndProc = win_proc,
+        .hInstance = instance,
+        .hCursor = LoadCursor(0, IDC_CROSS)
+    };
     
-    const char ClassName[] = "MyWindowClass";
+    ATOM atom = RegisterClassW(&wc);
+    Assert(atom && "Failed to register a window");
     
-    WindowClass.lpfnWndProc = WindowProc;
-    WindowClass.hInstance = Instance;
-    WindowClass.lpszClassName = ClassName;
-    WindowClass.hCursor = LoadCursor(0, IDC_CROSS);
+    HWND window = CreateWindowW(wc.lpszClassName, 
+                                L"Drawing Pixels", 
+                                WS_OVERLAPPEDWINDOW, 
+                                CW_USEDEFAULT, CW_USEDEFAULT, 
+                                CW_USEDEFAULT, CW_USEDEFAULT, 
+                                NULL, NULL, instance, NULL);
+    Assert(window && "Failed to create a window");
     
-    if(!RegisterClass(&WindowClass)) {
-        MessageBox(0, "RegisterClass failed", 0, 0);
-        return GetLastError();
-    }
+    ShowWindow(window, cmd_show);
     
-    HWND Window = CreateWindowEx(0,
-                                 ClassName,
-                                 "Window",
-                                 WS_OVERLAPPEDWINDOW|WS_VISIBLE,
-                                 CW_USEDEFAULT,
-                                 CW_USEDEFAULT,
-                                 CW_USEDEFAULT,
-                                 CW_USEDEFAULT,
-                                 0,
-                                 0,
-                                 Instance,
-                                 0);
+    // Allocate memory.
     
-    if(!Window) {
-        MessageBox(0, "CreateWindowEx failed", 0, 0);
-        return GetLastError();
-    }
+    RECT rect;
+    GetClientRect(window, &rect);
+    client_width = rect.right - rect.left;
+    client_height = rect.bottom - rect.top;
     
-    // Allocate memory
-    
-    RECT Rect;
-    GetClientRect(Window, &Rect);
-    ClientWidth = Rect.right - Rect.left;
-    ClientHeight = Rect.bottom - Rect.top;
-    
-    Memory = VirtualAlloc(0, 
-                          ClientWidth * ClientHeight * 4,
+    memory = VirtualAlloc(0, 
+                          client_width * client_height * 4,
                           MEM_RESERVE|MEM_COMMIT,
                           PAGE_READWRITE
                           );
     
-    // Create BITMAPINFO struct for StretchDIBits
+    // Create BITMAPINFO struct for StretchDIBits.
     
-    BITMAPINFO BitmapInfo;
-    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-    BitmapInfo.bmiHeader.biWidth = ClientWidth;
-    BitmapInfo.bmiHeader.biHeight = ClientHeight;
-    BitmapInfo.bmiHeader.biPlanes = 1;
-    BitmapInfo.bmiHeader.biBitCount = 32;
-    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    BITMAPINFO bitmap_info = {
+        .bmiHeader.biSize = sizeof(bitmap_info.bmiHeader),
+        .bmiHeader.biWidth = client_width,
+        .bmiHeader.biHeight = client_height,
+        .bmiHeader.biPlanes = 1,
+        .bmiHeader.biBitCount = 32,
+        .bmiHeader.biCompression = BI_RGB
+    };
     
-    HDC HDC = GetDC(Window);
+    HDC hdc = GetDC(window);
     
-    int Running = 1;
-    
-    while(Running) {
-        MSG Message;
-        while(PeekMessage(&Message, NULL, 0, 0, PM_REMOVE)) {
-            if(Message.message == WM_QUIT) {
-                Running = 0;
-            }
-            TranslateMessage(&Message);
-            DispatchMessage(&Message);
-        }
+    for(;;) {
+        MSG msg;
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if(msg.message == WM_QUIT) break;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+            continue;
+        } 
         
-        // Clear screen
+        clear_screen(0x333333);
         
-        ClearScreen(0x333333);
+        // Draw white pixel at 100, 100 (from bottom left).
         
-        // Draw white pixel at 100, 100 (from bottom left)
+        draw_pixel(100, 100, 0xffffff);
         
-        DrawPixel(100, 100, 0xffffff);
-        
-        StretchDIBits(HDC,
+        StretchDIBits(hdc,
                       0,
                       0,
-                      ClientWidth,
-                      ClientHeight,
+                      client_width,
+                      client_height,
                       0,
                       0,
-                      ClientWidth,
-                      ClientHeight,
-                      Memory,
-                      &BitmapInfo,
+                      client_width,
+                      client_height,
+                      memory,
+                      &bitmap_info,
                       DIB_RGB_COLORS,
                       SRCCOPY
                       );
